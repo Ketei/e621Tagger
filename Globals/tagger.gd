007e521ea701bb010e769194389b07deb3244177
Ketei <ketei.ilust@gmail.com>
2023-10-16 22:33:31 -0600

    Created picture folders and headers function

--- Globals/tagger.gd
+++ Globals/tagger.gd
@@ -20,14 +20,23 @@ enum Sites {
 	HYDRUS,
 }
 
-const e6_headers: Array = ["User-Agent: Taglist Maker/v0.3.2 (by Ketei)"] 
+
+var e6_headers_data: Dictionary = {
+	"User-Agent": "Taglist Maker/v0.3.2 (by Ketei)",
+}
+
+
 const implications_path: String = "user://database/implications/"
 const tags_path: String = "user://database/tags/"
+const tag_images_path: String = "user://database/tag_images/"
+const api_file_path: String = "user://e621_key.ini"
 
 var alias_database: AliasDatabase
 var settings: UserSettings
 var site_settings: SiteSettings
 var tag_manager: TagManager
+var settings_lists: SettingLists
+var headers_ini: ConfigFile
 
 
 func _init():
@@ -36,6 +45,7 @@ func _init():
 	settings = UserSettings.load_settings()
 	site_settings = SiteSettings.load_settings()
 	alias_database = AliasDatabase.load_database()
+	settings_lists = SettingLists.load_database()
 
 
 func verify_folder_structure() -> void:
@@ -43,9 +53,18 @@ func verify_folder_structure() -> void:
 	if not DirAccess.dir_exists_absolute("user://database"):
 		DirAccess.make_dir_absolute("user://database")
 	
-	if not DirAccess.dir_exists_absolute("user://database/tags"):
-		DirAccess.make_dir_absolute("user://database/tags")
+	if not DirAccess.dir_exists_absolute(tags_path):
+		DirAccess.make_dir_absolute(tags_path)
 	
-	if not DirAccess.dir_exists_absolute("user://database/implications"):
-		DirAccess.make_dir_absolute("user://database/implications")
+	if not DirAccess.dir_exists_absolute(implications_path):
+		DirAccess.make_dir_absolute(implications_path)
+	
+	if not DirAccess.dir_exists_absolute(tag_images_path):
+		DirAccess.make_dir_absolute(tag_images_path)
+		
 
+func get_headers() -> Array:
+	var return_headers: Array = []
+	for header_type in e6_headers_data.keys():
+		return_headers.append(header_type + ": " + e6_headers_data[header_type])
+	return return_headers
