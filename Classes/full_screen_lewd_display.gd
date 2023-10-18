f6d58edca9cc50fd4d1a40bca674fcdd281c13be
Ketei <ketei.ilust@gmail.com>
2023-10-18 13:12:52 -0600

    Fixed image display crash
    Changes expand modes.
    This fixed a crash that happened on resizing some images to fit the display.

--- Classes/full_screen_lewd_display.gd
+++ Classes/full_screen_lewd_display.gd
@@ -8,8 +8,12 @@ func _ready():
 	lewd_texture.hide_lewd_image.connect(hide_window)
 	self.hide()
 
-func set_picture(texture_for_rect: ImageTexture) -> void:
+
+func show_picture(texture_for_rect: ImageTexture) -> void:
 	lewd_texture.texture = texture_for_rect
+	if lewd_texture.expand_mode != TextureRect.EXPAND_IGNORE_SIZE:
+		lewd_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	show()
 
 
 func hide_window() -> void:
