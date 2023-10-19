7743c3eb08027a27d8c5a34b6925c227ff9aef77
Ketei <ketei.ilust@gmail.com>
2023-10-19 14:44:24 -0600

    Full screen zoom
    Full screen displays now zooms properly in the position clicked at.

--- Classes/full_screen_lewd_display.gd
+++ Classes/full_screen_lewd_display.gd
@@ -6,11 +6,16 @@ extends Control
 
 func _ready():
 	lewd_texture.hide_lewd_image.connect(hide_window)
-	self.hide()
+#	self.hide()
 
 
 func show_picture(texture_for_rect: ImageTexture) -> void:
 	lewd_texture.texture = texture_for_rect
+	
+	lewd_texture.max_h_range = texture_for_rect.get_width() - 1280
+	lewd_texture.max_v_range = texture_for_rect.get_height() - 720 
+	
+	
 	if lewd_texture.expand_mode != TextureRect.EXPAND_IGNORE_SIZE:
 		lewd_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	show()
