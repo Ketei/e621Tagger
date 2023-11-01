ce70e54450a634f31f14533201def0497df1cd7a
Ketei <ketei.ilust@gmail.com>
2023-10-31 23:35:00 -0600

    Tagger site setting
    Target now loads the default. Changing it doesn't change the settings

--- Scripts/platform_option_button.gd
+++ Scripts/platform_option_button.gd
@@ -6,13 +6,12 @@ extends OptionButton
 
 func _ready():
 	selected = Tagger.site_settings.target_site
-	
 	item_selected.connect(change_platform)
 
 
 func change_platform(index_value: int) -> void:
-	Tagger.site_settings.target_site = get_item_id(index_value) as Tagger.Sites
-	if not final_tag_list.text.is_empty():
+	tag_list_generator.target_site = get_item_id(index_value)
+	if not tagger.final_tag_list_array.is_empty():
 		final_tag_list.text = tag_list_generator.create_list_from_array(
 				tagger.final_tag_list_array
 		)
