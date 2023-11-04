f358246c1d3b7d122045affd263c31d955fa1749
Ketei <ketei.ilust@gmail.com>
2023-11-03 20:12:06 -0600

    API usage: One at a time

--- Scripts/api_interaction_script.gd
+++ Scripts/api_interaction_script.gd
@@ -7,3 +7,9 @@ extends Control
 func _ready():
 	load_api.successful_load.connect(log_in_api.clear_and_disable)
 	log_in_api.successful_log_in.connect(load_api.clear_and_disable)
+	
+	load_api.fields_disabled.connect(log_in_api.disable_fields)
+	load_api.fields_enabled.connect(log_in_api.enable_fields)
+	
+	log_in_api.fields_disabled.connect(load_api.disable_fields)
+	log_in_api.fields_enabled.connect(load_api.enable_fields)
