372e37717ee87bc1a2edafa0e7cda61ea223b9ec
Ketei <ketei.ilust@gmail.com>
2023-11-20 15:08:43 -0600

    Correct gender combination
    Now adding a gender/gender interaction will allow for any combination and set the correct one on save.
    
    Ex. female/male -> male/female

--- Scripts/gender_interact_item.gd
+++ Scripts/gender_interact_item.gd
@@ -2,7 +2,7 @@ extends HBoxContainer
 
 var final_selection: String = ""
 
-@onready var submissive_opt_btn: OptionButton = $ReceivingOptionButton
+@onready var submissive_opt_btn: OptionButton = $ReceivingGenderOptionButton
 @onready var accept_button: Button = $AcceptButton
 @onready var giving_label: Label = $GivingLabel
 @onready var receiving_label: Label = $ReceivingLabel
@@ -10,41 +10,72 @@ var final_selection: String = ""
 
 var edit_mode: bool = true
 
-
-var possible_interactions: Dictionary = {
-	"male": ["male", "female", "ambiguous"],
-	"female": ["female", "ambiguous"],
-	"andromorph": ["male", "female", "ambiguous", "andromorph", "gynomorph"],
-	"gynomorph": ["male", "female", "ambiguous", "gynomorph", "herm"],
-	"herm": ["male", "female", "herm", "andromorph", "ambiguous"],
-	"maleherm": ["male", "female", "ambiguous", "andromorph", "gynomorph", "herm", "maleherm"]
+const correct_interactions: Dictionary = {
+	"male/male": "male/male",
+	"male/female": "male/female",
+	"male/ambiguous": "male/ambiguous",
+	"male/andromorph": "andromorph/male",
+	"male/gynomorph": "gynomorph/male",
+	"male/herm": "herm/male",
+	"male/maleherm": "maleherm/male",
+	"female/male": "male/female",
+	"female/female": "female/female",
+	"female/ambiguous": "female/ambiguous",
+	"female/andromorph": "andromorph/female",
+	"female/gynomorph": "gynomorph/female",
+	"female/herm": "herm/female",
+	"female/maleherm": "maleherm/female",
+	"ambiguous/male": "male/ambiguous",
+	"ambiguous/female": "female/ambiguous",
+	"ambiguous/ambiguous": "ambiguous/ambiguous",
+	"ambiguous/andromorph": "andromorph/ambiguous",
+	"ambiguous/gynomorph": "gynomorph/ambiguous",
+	"ambiguous/herm": "herm/ambiguous",
+	"ambiguous/maleherm": "maleherm/ambiguous",
+	"andromorph/male": "andromorph/male",
+	"andromorph/female": "andromorph/female",
+	"andromorph/ambiguous": "andromorph/ambiguous",
+	"andromorph/andromorph": "andromorph/andromorph",
+	"andromorph/gynomorph": "gynomorph/andromorph",
+	"andromorph/herm": "herm/andromorph",
+	"andromorph/maleherm": "maleherm/andromorph",
+	"gynomorph/male": "gynomorph/male",
+	"gynomorph/female": "gynomorph/female",
+	"gynomorph/ambiguous": "gynomorph/ambiguous",
+	"gynomorph/andromorph": "gynomorph/andromorph",
+	"gynomorph/gynomorph": "gynomorph/gynomorph",
+	"gynomorph/herm": "gynomorph/herm",
+	"gynomorph/maleherm": "maleherm/gynomorph",
+	"herm/male": "herm/male",
+	"herm/female": "herm/female",
+	"herm/ambiguous": "herm/ambiguous",
+	"herm/andromorph": "herm/andromorph",
+	"herm/gynomorph": "gynomorph/herm",
+	"herm/herm": "herm/herm",
+	"herm/maleherm": "maleherm/herm",
+	"maleherm/male": "maleherm/male",
+	"maleherm/female": "maleherm/female",
+	"maleherm/ambiguous": "maleherm/ambiguous",
+	"maleherm/andromorph": "maleherm/andromorph",
+	"maleherm/gynomorph": "maleherm/gynomorph",
+	"maleherm/herm": "maleherm/herm",
+	"maleherm/maleherm": "maleherm/maleherm"
 }
 
 
+
 func _ready():
-	for gender in possible_interactions.keys():
-		giving_gender_option_button.add_item(gender)
-	giving_gender_option_button.select(-1)
-	
-	giving_gender_option_button.item_selected.connect(gender_selected)
 	accept_button.pressed.connect(save_pressed)
-
-
-func gender_selected(item_index: int) -> void:
-	submissive_opt_btn.clear()
-	for possibility in possible_interactions[giving_gender_option_button.get_item_text(item_index)]:
-		submissive_opt_btn.add_item(possibility)
-	submissive_opt_btn.select(0)
-	if submissive_opt_btn.disabled:
-		submissive_opt_btn.disabled = false
 
 
 func save_pressed() -> void:
 	if edit_mode:
-		final_selection = giving_gender_option_button.get_item_text(giving_gender_option_button.selected) + "/" + submissive_opt_btn.get_item_text(submissive_opt_btn.selected)
+		var string_make: String = giving_gender_option_button.get_item_text(giving_gender_option_button.selected) + "/" + submissive_opt_btn.get_item_text(submissive_opt_btn.selected)
+		final_selection = correct_interactions[string_make]
 		accept_button.text = "x"
-		giving_label.text = giving_gender_option_button.get_item_text(giving_gender_option_button.selected)
-		receiving_label.text = submissive_opt_btn.get_item_text(submissive_opt_btn.selected)
+		var giv_receive: Array = final_selection.split("/")
+		giving_label.text = giv_receive[0]
+		receiving_label.text = giv_receive[1]
 		giving_label.show()
 		receiving_label.show()
 		giving_gender_option_button.hide()
