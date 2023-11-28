1c1fadce786fa1d851ba1b4b15b48867b69130ff
Ketei <ketei.ilust@gmail.com>
2023-11-27 19:27:35 -0600

    Set tag property UI change
    Category no longer gets cut down
    You can now also set priority

--- Scripts/set_as_tag_script.gd
+++ Scripts/set_as_tag_script.gd
@@ -1,28 +1,66 @@
 extends Control
 
 
-signal category_selected(category_number)
+signal set_as_accepted
 
-@onready var tag_label: Label = $CenterContainer/SetWindow/VBoxContainer/HBoxContainer/TagLabel
-@onready var categories_menu: OptionButton = $CenterContainer/SetWindow/VBoxContainer/HBoxContainer/CategoriesMenu
-@onready var cancel_button: Button = $CenterContainer/SetWindow/VBoxContainer/ButtonsContainer/CancelButton
-@onready var accept_button: Button = $CenterContainer/SetWindow/VBoxContainer/ButtonsContainer/AcceptButton
+@onready var tag_label: Label = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/HBoxContainer/TagLabel
+@onready var categories_menu: OptionButton = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/HBoxContainer/CategoriesMenu
+@onready var cancel_button: Button = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/ButtonsContainer/CancelButton
+@onready var accept_button: Button = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/ButtonsContainer/AcceptButton
+@onready var change_cat_check_box: CheckBox = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/HBoxContainer/ChangeCatCheckBox
+@onready var change_prio_check_box: CheckBox = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/PrioHBox/ChangePrioCheckBox
+@onready var new_priority_spin_box: SpinBox = $CenterContainer/SetWindow/MarginContainer/VBoxContainer/PrioHBox/NewPrioritySpinBox
+
+var prio_selected: int = 0
+var change_prio: bool = false
+
+var category_select := Tagger.Categories.GENERAL
+var change_category: bool = false
 
 
 func _ready():
 	hide()
 	accept_button.pressed.connect(accept_category)
 	cancel_button.pressed.connect(cancel_category)
+	
+	new_priority_spin_box.value_changed.connect(on_spinbox_value_change)
+	categories_menu.item_selected.connect(on_cat_change)
+	
+	change_cat_check_box.toggled.connect(on_cat_toggle)
+	change_prio_check_box.toggled.connect(on_prio_toggle)
+
+
+func on_cat_toggle(is_toggled:bool) -> void:
+	change_category = is_toggled
+
+
+func on_prio_toggle(is_toggled:bool) -> void:
+	change_prio = is_toggled
+
+
+func on_spinbox_value_change(new_value: float) -> void:
+	prio_selected = int(new_value)
+	if not change_prio_check_box.button_pressed:
+		change_prio_check_box.button_pressed = true
+
+
+func on_cat_change(new_category: int) -> void:
+	category_select = new_category as Tagger.Categories
+	if not change_cat_check_box.button_pressed:
+		change_cat_check_box.button_pressed = true
 
 
 func set_target_tag(tag_name: String) -> void:
 	tag_label.text = tag_name
 	categories_menu.select(0)
+	new_priority_spin_box.set_value_no_signal(0)
+	change_prio_check_box.button_pressed = false
+	change_cat_check_box.button_pressed = false
 
 
 func accept_category() -> void:
-	category_selected.emit(categories_menu.selected)
+	set_as_accepted.emit()
 
 
 func cancel_category() -> void:
-	category_selected.emit(-1)
+	set_as_accepted.emit()
