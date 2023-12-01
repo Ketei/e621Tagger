6d9eb4eb1c25cf8fcf102d3858e7111e284abcaa
Ketei <ketei.ilust@gmail.com>
2023-11-30 18:08:30 -0600

    Tagger can now play (ogv) videos
    No, I can't use any other. It's Godot's limitation.
    Also no media seeker.

---
+++ Scripts/video_thumbnail.gd
@@ -0,0 +1,18 @@
+class_name VideoThumbnail
+extends TextureRect
+
+
+signal thumbnail_clicked(path_to_file: String)
+
+var video_path: String = ""
+
+
+func _ready():
+	gui_input.connect(gui_input_received)
+
+
+func gui_input_received(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		thumbnail_clicked.emit(video_path)
+		#print("Click")
+		
