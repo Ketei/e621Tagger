3078ff42b228bc29bcfafa7ac3ee4d6b99119845
Ketei <ketei.ilust@gmail.com>
2023-12-07 16:09:44 -0600

    Cleaned comments

--- Classes/lewd_display_thumbnail.gd
+++ Classes/lewd_display_thumbnail.gd
@@ -3,24 +3,18 @@ extends TextureRect
 
 signal lewd_pic_pressed(image_texture)
 
-#@onready var button = $Button
 
 func _ready():
 	gui_input.connect(gui_input_received)
-#
-#
-#func button_pressed() -> void:
-	#lewd_pic_pressed.emit(self.texture)
-	#button.call_deferred("release_focus")
 
 
 func gui_input_received(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		lewd_pic_pressed.emit(self.texture)
-		
 
 
 func pause_texture(is_paused: bool) -> void:
 	if texture is AnimatedTexture:
 		if texture.pause != is_paused:
 			texture.pause = is_paused
+
