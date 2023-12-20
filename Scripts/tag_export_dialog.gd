91724f74092e29e92365b3c4d69883b6e62b0d83
Ketei <ketei.ilust@gmail.com>
2023-12-19 18:59:48 -0600

    Exporting a file now validates name.
    Now when exporting a file it'll check if the filename is valid, if not it'll change it.

--- Scripts/tag_export_dialog.gd
+++ Scripts/tag_export_dialog.gd
@@ -12,17 +12,31 @@ func _ready():
 
 
 func show_dialog() -> void:
-	if not Tagger.settings.default_save_path.is_empty() and DirAccess.dir_exists_absolute(Tagger.settings.default_save_path):
+	if not default_path.is_empty() and DirAccess.dir_exists_absolute(default_path):
+		current_path = default_path
+	elif not Tagger.settings.default_save_path.is_empty() and DirAccess.dir_exists_absolute(Tagger.settings.default_save_path):
 		current_path = Tagger.settings.default_save_path
 	current_file = default_filename
 	show()
 
 
 func file_saved(path: String) -> void:
-	var test_file = FileAccess.open(path, FileAccess.WRITE)
-	test_file.store_string(tag_string)
+	var folder_path: String = path.get_base_dir()
+	if not folder_path.ends_with("/"):
+		folder_path += "/"
+	var file_name: String = path.get_file().validate_filename()
+	
+	var test_file = FileAccess.open(folder_path + file_name, FileAccess.WRITE)
+	if test_file != null:
+		test_file.store_string(tag_string)
+	else:
+		print_debug("Couldn't create file. Error: " + str(FileAccess.get_open_error()))
+	
 	test_file.close()
 	
-	default_path = path.get_base_dir() + "/"
-	default_filename = path.get_file().get_basename()
+	if default_path != folder_path:
+		default_path = folder_path
+	if default_filename != file_name:
+		default_filename = file_name
+	default_filename = file_name
 
